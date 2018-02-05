5565715005de03a788642fe40ef660e80581d72d
Cesar Santana <casc.santana@gmail.com>
2018-02-04 21:38:52 -0600

    Creating files to make the app work with .env file

---
+++ enviroment.js
@@ -0,0 +1,20 @@
+var glob = require('glob')
+var dotenv = require('dotenv').config()
+var fs = require('fs')
+var beautify = require('json-beautify')
+
+var target = './src/config'
+var filename = '/target.json'
+
+glob(target, function (er, files) {
+  let stringFile = beautify(dotenv.parsed, null, 2, 80)
+  fs.writeFile(target + filename, stringFile, function (err) {
+    if (err) {
+      console.log('there was an error saving files ', err)
+    }
+    else {
+      console.log('Creating paths for each component...')
+      console.log(target + filename + ' has been generated')
+    }
+  })
+})
