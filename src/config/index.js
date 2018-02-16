411c54808084607fd4ff2a74338091762b9a02b8
Cesar Santana <casc.santana@gmail.com>
2018-02-16 14:30:52 -0600

    Implementing helper env in config

--- src/config/index.js
+++ src/config/index.js
@@ -1,25 +1,22 @@
-export const STATUS = typeof window.env != 'undefined' ? window.env : 'local' // eslint-disable-line
-
+import Env from 'env'
+export let STATUS = Env.SERVER
 export const BASE_URL = {
   'local': 'http://localhost:8000/',
-  // 'local': 'http://10.0.0.7:8000/',
   'demo': 'http://api.cesarsantana.ml/',
   'example': 'https://jsonplaceholder.typicode.com/',
   'beta': 'https://beta.carzuz.com/',
   'test': 'https://carzuz.bycarbono.tech/'
 }
-
 function apiSecret (value = '') {
   return `API_${value.toUpperCase()}_SECRET`
 }
-
 let secrets = {
-  API_LOCAL_SECRET: '8h7igsTOIk08ngWTiWE3J4WiCDjyZI2vZbV2VMOl',
-  API_BETA_SECRET: 'ohogsfw7mkb0Mx2sRj0YQq8HH86DiYRBnHcTIASx',
-  API_TEST_SECRET: 'ohogsfw7mkb0Mx2sRj0YQq8HH86DiYRBnHcTIASx'
+  API_LOCAL_SECRET: Env.API_LOCAL_SECRET,
+  API_BETA_SECRET: Env.API_BETA_SECRET,
+  API_TEST_SECRET: Env.API_TEST_SECRET
 }
 
-export const DEFAULT_LOCALE = 'en'
+export const DEFAULT_LOCALE = Env.LOCALE
 export const FALLBACK_LOCALE = 'en'
 export const API_BASE_URL = BASE_URL[STATUS]
 export const API_URL = BASE_URL[STATUS] + 'api/'
@@ -30,7 +27,7 @@ let secret = apiSecret(STATUS)
 export default {
   oauth: {
     grant_type: 'password',
-    client_id: 2,
+    client_id: Env.CLIENT_ID,
     client_secret: secrets[secret],
     scope: '*'
   }
